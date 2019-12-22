778a2c4951142b20b9eaea6820f03fee0703488b
Julien Chameroy <julien.chameroy@gmail.com>
2019-12-22 05:52:52 -0800

    test static list

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>
+        <ul>
+          <li>test 1</li>
+          <li>test 2</li>
+          <li>test 3</li>
+          <li>test 4</li>
+          <li>test 5</li>
+          <li>test 6</li>
+        </ul>
+      </h2>
     </div>
   );
 }
