55093d5e609a4e7371ea882f083cc2be02f9a3cd
Julien Chameroy <julien.chameroy@gmail.com>
2019-12-22 06:35:03 -0800

    ajout flexbox

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>
-        <ul>
-          <li>test 1</li>
-          <li>test 2</li>
-          <li>test 3</li>
-          <li>test 4</li>
-          <li>test 5</li>
-          <li>test 6</li>
-        </ul>
-      </h2>
+      <h1>Hello React</h1>
+      <div class="container">
+        <div>test 1</div>
+        <div>test 2</div>
+        <div>test 3</div>
+        <div>test 4</div>
+        <div>test 5</div>
+        <div>test 6</div>
+      </div>
     </div>
   );
 }
